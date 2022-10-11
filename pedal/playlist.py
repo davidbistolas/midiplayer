76351e8d12d0dba2f1ec13d3f20cfe4db5d5b1bb
David Bistolas <david.bistolas@gmail.com>
2022-10-10 20:02:56 -0400

    Update the player

--- pedal/playlist.py
+++ pedal/playlist.py
@@ -1,6 +1,5 @@
-import os
-
 import gc
+import os
 
 
 class Playlist:
@@ -8,8 +7,8 @@ class Playlist:
     Manage the playlist
     """
 
-    def __init__(self, path="/midi", playlist_file='playlist.txt'):
-
+    def __init__(self, path="/storage", playlist_file='playlist.txt'):
+        """ Init the Playlist object."""
         sd_contents = os.listdir(path)
         playlist = []
         self.path = path
@@ -38,23 +37,29 @@ class Playlist:
         self._current_song_index = 0
 
     def goto_next_song(self):
+        """Go to the next song in the playlist"""
         if self._current_song_index < (len(self._playlist) - 1):
             self._current_song_index += 1
 
     def goto_previous_song(self):
+        """Go to the previous song in the playlist"""
         if self._current_song_index > 0:
             self._current_song_index -= 1
 
     def get_current_song(self):
+        """Return the current song"""
+
         return self._playlist[self._current_song_index]
 
     def get_previous_song(self):
+        """Return the previous song"""
         if self._current_song_index > 0:
             return self._playlist[self._current_song_index - 1]
         else:
             return "                              "
 
     def get_next_song(self):
+        """Return the next song"""
         if self._current_song_index < (len(self._playlist) - 1):
             return self._playlist[self._current_song_index + 1]
         else:
